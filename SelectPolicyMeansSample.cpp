6c35dc606649b7c618deeda1ae911a49d0316664
kspace-trk <keigonakao.0825@gmail.com>
2020-09-30 10:29:27 +0900

    100,200,10に変更

--- SelectPolicyMeansSample.cpp
+++ SelectPolicyMeansSample.cpp
@@ -6,33 +6,22 @@ int main()
 {
 	Exp *exp;
 	Population *pop;
-	int n;
-	double e = 0;//評価値
+
 	long cpu_time;
 	double sec;
 	// 乱数のタネ設定
 	srand((unsigned int)time(NULL));
 
-	double geneAve[100];
-	double ave = 0;
 
-	printf("開始\n");
 
 	exp = new Exp();
-	// for (n = 0; n < 10; n++)
-	// {
+
 	exp->search();
-	 //		ave += exp->pop->ind[0]->fitness;
-	 //		printf("-----------------\n");
-	//	 }
-	 //ave /= n;
+
 	printf("世代交代数：%d　個体群数：%d \n", ITERATION_NUM, POP_SIZE);
-	printf("第%d世代：fitness=%f\n", exp->pop->iteNum, exp->pop->ind[0]->fitness);
 	cpu_time = clock();
 	sec = (double)cpu_time / CLOCKS_PER_SEC;
-	e = sqrt(pow(exp->pop->ind[0]->fitness / 100, 2.0) + pow(sec / 70, 2.0));
 	printf("処理時間: %f秒\n", sec);
-	printf("評価値：%f\n", e);
 	exp->pop->ind[0]->writeResult();
 	delete exp;
 }
