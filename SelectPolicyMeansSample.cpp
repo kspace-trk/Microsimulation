c8fd85b1d8e1698d83f2bc9dcd620240322179cb
kspace-trk <keigonakao.0825@gmail.com>
2020-10-05 12:27:45 +0900

    [add]平均値

--- SelectPolicyMeansSample.cpp
+++ SelectPolicyMeansSample.cpp
@@ -14,17 +14,23 @@ int main()
 
 	double geneAve[100];
 	double ave = 0;
+	double subsidyAve = 0;
+	double facilityAve = 0;
 
 	exp = new Exp();
-	// for (n = 0; n < 50; n++)
-	// {
-	exp->search();
-	// 	ave += exp->pop->ind[0]->fitness;
-	// 	printf("第%d世代：fitness=%f\n", exp->pop->iteNum, exp->pop->ind[0]->fitness);
-	// 	printf("-----------------\n");
-	// }
-	// ave /= n;
-	printf("世代交代数：%d　個体群数：%d　\n", ITERATION_NUM, POP_SIZE);
+	for (n = 0; n < 10; n++)
+	{
+		exp->search();
+		ave += exp->pop->ind[0]->fitness;
+		subsidyAve += exp->pop->ind[0]->subsidy;
+		facilityAve += exp->pop->ind[0]->facilityNum;
+		printf("第%d世代：fitness=%f\n", exp->pop->iteNum, exp->pop->ind[0]->fitness);
+		printf("-----------------\n");
+	}
+	ave /= n;
+	subsidyAve /= n;
+	facilityAve /= n;
+	printf("世代交代数：%d　個体群数：%d　平均：%f　補助金平均：%f　施設数平均：%f\n", ITERATION_NUM, POP_SIZE, ave, subsidyAve, facilityAve);
 	cpu_time = clock();
 	sec = (double)cpu_time / CLOCKS_PER_SEC;
 	printf("処理時間: %f秒\n", sec);
